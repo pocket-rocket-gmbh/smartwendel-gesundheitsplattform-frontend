5af8431f76ecad7954f63ce22b0724a9f0ca0c66
NormanF <nf@pocket-rocket.io>
2023-07-25 11:34:12 +0200

    added to changelog

--- pages/admin/index.vue
+++ pages/admin/index.vue
@@ -69,6 +69,15 @@
       </v-list-item>
     </v-list>
     <v-divider></v-divider>
+    <v-list>
+      <v-list-item title="Änderungen am: 25.07 11:30Uhr - Norman">
+        <v-container>
+          - Tabellen sind sortierbar<br>
+        
+        </v-container>
+      </v-list-item>
+    </v-list>
+    <v-divider></v-divider>
   </div>
 </template>
 
