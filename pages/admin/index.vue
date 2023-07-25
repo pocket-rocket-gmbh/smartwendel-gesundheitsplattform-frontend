42f9ac2d021a4d1cdb19f5db392c900e570c3b8f
NormanF <nf@pocket-rocket.io>
2023-07-25 10:58:49 +0200

    updated changelog

--- pages/admin/index.vue
+++ pages/admin/index.vue
@@ -72,6 +72,15 @@
       </v-list-item>
     </v-list>
     <v-divider></v-divider>
+    <v-list>
+      <v-list-item title="Änderungen am: 25.07 11:00Uhr - Norman">
+        <v-container>
+          - Tabellen sind durchsuchbar<br>
+        
+        </v-container>
+      </v-list-item>
+    </v-list>
+    <v-divider></v-divider>
   </div>
 </template>
 
