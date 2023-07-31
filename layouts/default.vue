a4b00661711234142c4e922718a94c8ea2dbfea5
donmb1 <martinbr83@gmail.com>
2023-07-31 13:48:09 +0200

    password protect default layout

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,15 +1,20 @@
 <template>
   <v-app>
-    <ClientOnly>
-      <ClientSnackbar />
-    </ClientOnly>
-    <div>
-      <PublicLayoutsTopBar />
-      <v-main>
-        <slot />
-      </v-main>
-      <PublicLayoutsFooter />
-      <PublicLoginPanel  v-if="!useUser().loggedIn()"/>
+    <PublicPasswordProtection
+      v-if="!authenticated"
+    />
+    <div v-else>
+      <ClientOnly>
+        <ClientSnackbar />
+      </ClientOnly>
+      <div>
+        <PublicLayoutsTopBar />
+        <v-main>
+          <slot />
+        </v-main>
+        <PublicLayoutsFooter />
+        <PublicLoginPanel  v-if="!useUser().loggedIn()"/>
+      </div>
     </div>
   </v-app>
 </template>
@@ -18,28 +23,28 @@
 import { useAuthStore } from "@/store/auth";
 import { useTooltipsStore } from '~~/store/tooltips'
 
-onMounted(() => {
-  const auth = localStorage.getItem("smartwendel_gesundheitsplattform_authenticated");
-  if (auth && auth === "true") {
-    useAuthStore().$patch({
-      authenticated: true,
-    });
-  }
-});
+  onMounted(() => {
+    const auth = localStorage.getItem("smartwendel_gesundheitsplattform_authenticated");
+    if (auth && auth === "true") {
+      useAuthStore().$patch({
+        authenticated: true,
+      });
+    }
+  });
 
   const tooltipsStore = useTooltipsStore()
   const api = useCollectionApi()
   api.setBaseApi(usePublicApi())
   api.setEndpoint('tooltips')
 
-    const getTooltips = async () => {
-      await api.retrieveCollection()
-      tooltipsStore.tooltips = api.items
-    }
+  const getTooltips = async () => {
+    await api.retrieveCollection()
+    tooltipsStore.tooltips = api.items
+  }
 
-    onMounted(() => {
-        getTooltips()
-      })
+  onMounted(() => {
+    getTooltips()
+  })
 
   const authenticated = computed(() => {
     return useAuthStore().authenticated;
