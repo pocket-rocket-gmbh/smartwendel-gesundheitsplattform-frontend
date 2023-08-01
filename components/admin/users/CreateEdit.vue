ea7fb74bcd0e09aa1ab12949efab2cfb8ffccf7c
NormanF <nf@pocket-rocket.io>
2023-08-01 09:34:26 +0200

    fixed admin users-createedit role selection

--- components/admin/users/CreateEdit.vue
+++ components/admin/users/CreateEdit.vue
@@ -1,28 +1,13 @@
 <template>
-  <v-dialog
-    v-model="dialog"
-    transition="dialog-top-transition"
-    width="900"
-    @click:outside="emitClose()"
-  >
+  <v-dialog v-model="dialog" transition="dialog-top-transition" width="900" @click:outside="emitClose()">
     <v-card class="dialog-900">
-      <v-card-title class="text-h5">
-        Benutzer
-      </v-card-title>
+      <v-card-title class="text-h5"> Benutzer </v-card-title>
       <v-card-text>
         <div class="field">
-          <v-text-field
-            v-model="item.firstname" 
-            label="Vorname"
-            hide-details="auto"
-          />
+          <v-text-field v-model="item.firstname" label="Vorname" hide-details="auto" />
         </div>
         <div class="field">
-          <v-text-field
-            v-model="item.lastname"
-            label="Nachname"
-            hide-details="auto"
-          />
+          <v-text-field v-model="item.lastname" label="Nachname" hide-details="auto" />
         </div>
         <div class="field">
           <v-text-field
@@ -53,18 +38,8 @@
         </div>
       </v-card-text>
       <v-card-actions>
-        <v-btn
-          @click="emitClose()"
-        >
-          Schließen
-        </v-btn>
-        <v-btn
-          color="blue darken-1"
-          variant="outlined"
-          dark
-          @click="handleCta()"
-          :loading="loadingItem"
-        >
+        <v-btn @click="emitClose()"> Schließen </v-btn>
+        <v-btn color="blue darken-1" variant="outlined" dark @click="handleCta()" :loading="loadingItem">
           <span v-if="itemId">Speichern</span>
           <span v-else>Einladen</span>
         </v-btn>
@@ -73,105 +48,108 @@
   </v-dialog>
 </template>
 
-<script lang="ts">
-import { ResultStatus } from '@/types/serverCallResult'
-import axios from 'axios'
-export default defineComponent({
-  emits: ['close', 'refreshCollection'],
-  props: {
-    itemId: String
-  },
-  setup (props, { emit }) {
-    const loadingItem = ref(false)
-    const dialog = ref(true)
-    const errors = ref([])
+<script setup lang="ts">
+import { User } from "~/store/searchFilter";
+import { ResultStatus } from "@/types/serverCallResult";
+import axios from "axios";
 
-    const item = ref({
-      name: '',
-      register_token: ''
-    })
-    const roles = ref([
-      { name: 'Einrichtung', id: 'facility_owner'},
-      { name: 'Standard Benutzer', id: 'user'},
-      { name: 'Super Administrator', id: 'care_facility_admin'},
-      { name: 'Administrator', id: 'admin'}
-    ])
+const emit = defineEmits(["close", "refreshCollection"]);
+const props = defineProps<{
+  itemId: string;
+}>();
 
-    const showApi = useCollectionApi()
-    showApi.setBaseApi(usePrivateApi())
+const loadingItem = ref(false);
+const dialog = ref(true);
+const errors = ref([]);
 
-    const createUpdateApi = useCollectionApi()
-    createUpdateApi.setBaseApi(usePrivateApi())
+const item = ref<User>({} as User);
 
-    const getItem = async () => {
-      showApi.setEndpoint(`users/${props.itemId}`)
+const user = useUser();
 
-      loadingItem.value = true
-      await showApi.getItem()
-      loadingItem.value = false
-      item.value = showApi.item.value
-    }
+const allRoles = [
+  { name: "Einrichtung", id: "facility_owner" },
+  { name: "Standard Benutzer", id: "user" },
+  { name: "Administrator", id: "admin", condition: "admin" },
+  { name: "Super Administrator", id: "care_facility_admin", condition: "care_facility_admin" },
+];
 
-    const handleCta = () => {
-      if (props.itemId) {
-        save()
-      } else {
-        invite()
-      }
-    }
+const roles = ref([]);
 
-    const invite = async () => {
-      createUpdateApi.setEndpoint(`users/invite`)
-      loadingItem.value = true
+const showApi = useCollectionApi();
+showApi.setBaseApi(usePrivateApi());
 
-      const res = await axios.post("/api/get_register_token");
-      item.value.register_token = res.data
+const createUpdateApi = useCollectionApi();
+createUpdateApi.setBaseApi(usePrivateApi());
 
-      const result = await createUpdateApi.createItem(item.value, 'Benutzer erfolgreich eingeladen')
-      loadingItem.value = false
-      if (result.status === ResultStatus.SUCCESSFUL) {
-        emit('refreshCollection')
-        emit('close')
-      } else {
-        errors.value = result.data
-      }
-    }
+const getItem = async () => {
+  showApi.setEndpoint(`users/${props.itemId}`);
 
-    const save = async () => {
-      createUpdateApi.setEndpoint(`users/${props.itemId}`)
-      loadingItem.value = true
-      const result = await createUpdateApi.updateItem(item.value, 'Benutzer erfolgreich aktualisiert')
-      loadingItem.value = false
-      if (result.status === ResultStatus.SUCCESSFUL) {
-        emit('refreshCollection')
-        emit('close')
-      } else {
-        errors.value = result.data
-      }
-    }
+  loadingItem.value = true;
+  await showApi.getItem();
+  loadingItem.value = false;
+  item.value = showApi.item.value;
+};
 
-    onMounted(() => {
-      if (props.itemId) {
-        getItem()
-      }
-    })
-    
-    const emitClose = () => {
-      emit('close')
-    }
-
-    return {
-      loadingItem,
-      dialog,
-      item,
-      handleCta,
-      emitClose,
-      roles,
-      errors
-    }
+const handleCta = () => {
+  if (props.itemId) {
+    save();
+  } else {
+    invite();
   }
-})
+};
+
+const invite = async () => {
+  createUpdateApi.setEndpoint(`users/invite`);
+  loadingItem.value = true;
+
+  const res = await axios.post("/api/get_register_token");
+  item.value.register_token = res.data;
+
+  const result = await createUpdateApi.createItem(item.value, "Benutzer erfolgreich eingeladen");
+  loadingItem.value = false;
+  if (result.status === ResultStatus.SUCCESSFUL) {
+    emit("refreshCollection");
+    emit("close");
+  } else {
+    errors.value = result.data;
+  }
+};
+
+const save = async () => {
+  createUpdateApi.setEndpoint(`users/${props.itemId}`);
+  loadingItem.value = true;
+  const result = await createUpdateApi.updateItem(item.value, "Benutzer erfolgreich aktualisiert");
+  loadingItem.value = false;
+  if (result.status === ResultStatus.SUCCESSFUL) {
+    emit("refreshCollection");
+    emit("close");
+  } else {
+    errors.value = result.data;
+  }
+};
+
+onMounted(() => {
+  if (props.itemId) {
+    getItem();
+  }
+
+  roles.value = allRoles
+    .map((role) => {
+      if (
+        !role.condition ||
+        (role.condition === "admin" && user.isAdmin()) ||
+        (role.condition === "care_facility_admin" && user.isSuperAdmin())
+      )
+        return role;
+    })
+    .filter(Boolean);
+
+  console.log(roles.value);
+});
+
+const emitClose = () => {
+  emit("close");
+};
 </script>
 
-<style lang="sass">
-</style>
+<style lang="sass"></style>
