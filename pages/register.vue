743dc45c91f54336de84172b0ccfb70e70cb1b00
NormanF <nf@pocket-rocket.io>
2023-08-04 12:17:40 +0200

    fixed footer not on bottom

--- pages/register.vue
+++ pages/register.vue
@@ -3,7 +3,7 @@
     <v-col sm="3" md="4" offset-sm="4">
       <v-card elevation="10" :class="['pa-6', {'shake' : animated}]">
         <img class="is-fullwidth" src="~/assets/images/logo.png" />
-        <div v-if="!registerSuccessful" class="mt-3">
+        <div v-if="registerSuccessful" class="mt-3">
           <div class="field">
             <v-text-field
               v-model="careFacilityName"
@@ -86,12 +86,12 @@
               <div>
                 Ich stimme der
                 <v-tooltip bottom>
-                  <template v-slot:activator="{ on }">
+                  <template v-slot:activator="{ props }">
                     <a
                       target="_blank"
                       href="/privacy_policy"
                       @click.stop
-                      v-on="on"
+                      v-bind="props"
                     >
                       <u>Datenschutzerklärung</u>
                     </a>
@@ -100,12 +100,12 @@
                 </v-tooltip>
                 <span> und den </span>
                 <v-tooltip bottom>
-                  <template v-slot:activator="{ on }">
+                  <template v-slot:activator="{ props }">
                     <a
                       target="_blank"
                       href="/terms_of_use"
                       @click.stop
-                      v-on="on"
+                      v-bind="props"
                     >
                       <u>Nutzungsbedingungen</u>
                     </a>
