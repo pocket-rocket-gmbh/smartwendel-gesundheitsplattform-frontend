9648fe6316fc5aa9be37c1d5866b1193e2c754f5
NormanF <nf@pocket-rocket.io>
2023-08-04 12:14:27 +0200

    fixed currentrecommendations alignment

--- components/public/landing/CurrentRecommendations.vue
+++ components/public/landing/CurrentRecommendations.vue
@@ -13,7 +13,7 @@
         :key="index"
       >
         <img class="is-clickable icons" :src="item.content.image" width="120" />
-        <div class="has-font-size-medium font-weight-bold">
+        <div class="font-weight-bold">
           <a :href="item.content.link" target="_blank" class="is-white is-clickable">
             {{ item.content.heading }}
           </a>
@@ -22,82 +22,86 @@
     </div>
   </div>
 </template>
-<script lang="ts">
-import { defineComponent } from "vue";
+<script setup lang="ts">
 import image1 from "@/assets/images/current-recommendations/icon_sport.svg";
 import image2 from "@/assets/images/current-recommendations/icon_nutrition.svg";
 import image3 from "@/assets/images/current-recommendations/icon_mental_health.svg";
 import image4 from "@/assets/images/current-recommendations/icon_health_course.svg";
 import image5 from "@/assets/images/current-recommendations/icon_free_time.svg";
-export default defineComponent({
-  setup() {
-    const items = [
-      {
-        content: {
-          heading: "Sport und Bewegung",
-          image: image1,
-          link: "www.google.com",
-        },
-      },
-      {
-        content: {
-          heading: "Ernährung",
-          image: image2,
-          link: "",
-        },
-      },
-      {
-        content: {
-          heading: "Mentale Gesundheit",
-          image: image3,
-          link: "",
-        },
-      },
-      {
-        content: {
-          heading: "Gesundheitskurse",
-          image: image4,
-          link: "",
-        },
-      },
-      {
-        content: {
-          heading: "Freizeitgestaltung",
-          image: image5,
-          link: "",
-        },
-      },
-    ];
 
-    return {
-      items,
-    };
+const items = [
+  {
+    content: {
+      heading: "Sport und Bewegung",
+      image: image1,
+      link: "www.google.com",
+    },
   },
-});
+  {
+    content: {
+      heading: "Ernährung",
+      image: image2,
+      link: "",
+    },
+  },
+  {
+    content: {
+      heading: "Mentale Gesundheit",
+      image: image3,
+      link: "",
+    },
+  },
+  {
+    content: {
+      heading: "Gesundheitskurse",
+      image: image4,
+      link: "",
+    },
+  },
+  {
+    content: {
+      heading: "Freizeitgestaltung",
+      image: image5,
+      link: "",
+    },
+  },
+];
 </script>
 
-<style lang="sass" scoped>
-@import "@/assets/sass/main.sass"
-.bg-gradient
-  background: linear-gradient(85.85deg, #91A80D 5.31%, #BAC323 35.73%, #9EA100 77.83%)
+<style lang="scss" scoped>
+@import "@/assets/sass/main.sass";
 
-.icons
-  border-radius: 50%
-  border: 1px solid white
-  &:hover
-    transform: scale(1.1)
-    transition: transform 0.5s ease-in-out
-    cursor: pointer
+.bg-gradient {
+  background: linear-gradient(85.85deg, #91a80d 5.31%, #bac323 35.73%, #9ea100 77.83%);
+}
+.icons {
+  border-radius: 50%;
+  border: 1px solid white;
+  &:hover {
+    transform: scale(1.1);
+    transition: transform 0.5s ease-in-out;
+    cursor: pointer;
+  }
+}
+.wrapper {
+  padding: 5rem;
 
-.wrapper
-  padding: 5rem
+  @include md {
+    padding: 1rem;
+  }
+}
+.recommendations-wrapper {
+  display: grid;
+  grid-template-columns: repeat(5, 1fr);
+  font-size: 22px;
 
-  @include md
-    padding: 1rem
+  @include md {
+    grid-template-columns: repeat(3, 1fr);
+    font-size: 18px;
+  }
 
-.recommendations-wrapper
-  display: flex
-  flex-wrap: wrap
-  gap: 1rem
-  justify-content: space-between
+  @include sm {
+    grid-template-columns: 1fr 1fr;
+  }
+}
 </style>
