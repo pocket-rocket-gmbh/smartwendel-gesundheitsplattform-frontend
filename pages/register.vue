a11e9da79a14d5d840e7956840d058c676a7b7e8
NormanF <nf@pocket-rocket.io>
2023-08-19 14:21:08 +0200

    improve register form

--- pages/register.vue
+++ pages/register.vue
@@ -1,15 +1,16 @@
 <template>
   <v-row class="my-15">
     <v-col sm="3" md="4" offset-sm="4">
-      <v-card elevation="10" :class="['pa-6', {'shake' : animated}]">
+      <v-card elevation="10" :class="['card', { shake: animated }]">
         <img class="is-fullwidth" src="~/assets/images/logo.png" />
-        <div v-if="!registerSuccessful" class="mt-3">
+        <v-form ref="registerForm" v-show="!registerSuccessful" class="mt-3">
           <div class="field">
             <v-text-field
               v-model="careFacilityName"
               type="text"
-              label="Name der Einrichtung/ Unternehmen/Verband/Verein/Behörde *"
+              label="Name der Einrichtung/Unternehmen/Verband/Verein/Behörde *"
               :error-messages="useErrors().checkAndMapErrors('firstname', errors)"
+              :rules="[rules.required]"
               hide-details="auto"
             />
           </div>
@@ -19,6 +20,7 @@
               type="text"
               label="Vorname *"
               :error-messages="useErrors().checkAndMapErrors('firstname', errors)"
+              :rules="[rules.required]"
               hide-details="auto"
             />
           </div>
@@ -28,72 +30,68 @@
               type="text"
               label="Nachname *"
               :error-messages="useErrors().checkAndMapErrors('lastname', errors)"
+              :rules="[rules.required]"
               hide-details="auto"
             />
           </div>
           <div class="field">
-            <v-text-field 
+            <v-text-field
               v-model="phone"
-              type="number"
+              type="tel"
               label="Telefonnummer *"
               hide-details="auto"
-              :rules="[rules.validateNumber]"
+              :rules="[rules.required, rules.validateNumber]"
               :error-messages="useErrors().checkAndMapErrors('phone', errors)"
             />
           </div>
           <div class="field">
-            <v-text-field 
+            <v-text-field
               v-model="email"
               type="email"
               label="E-Mail Adresse *"
+              :rules="[rules.required, rules.email]"
               hide-details="auto"
               :error-messages="useErrors().checkAndMapErrors('email', errors)"
             />
           </div>
-            <div class="field">
-              <v-select
-                hide-details="auto"
-                v-model="careFacilityCommunityId"
-                :items="communities"
-                item-title="name"
-                item-value="id"
-                label="Gemeinde"
-                :rules="[rules.required]"
-              />
-            </div>
-            <div class="field split">
-              <v-text-field
-                v-model="careFacilityZip"
-                hide-details="auto"
-                label="PLZ"
-                :type="'number'"
-                :rules="[rules.required, rules.zip]"
-                :error-messages="
-                  useErrors().checkAndMapErrors('zip', errors)
-                "
-              />
-              <v-select
-                hide-details="auto"
-                v-model="careFacilityTown"
-                :items="getTownsByCommunityId(careFacilityCommunityId)"
-                item-title="name"
-                item-value="name"
-                label="Ort"
-                :rules="[rules.required]"
-              />
-            </div>
-          <v-checkbox v-model="privacyAccepted">
+          <!-- <div class="field">
+            <v-select
+              hide-details="auto"
+              v-model="careFacilityCommunityId"
+              :items="communities"
+              item-title="name"
+              item-value="id"
+              label="Gemeinde"
+              :rules="[rules.required]"
+            />
+          </div>
+          <div class="field split">
+            <v-text-field
+              v-model="careFacilityZip"
+              hide-details="auto"
+              label="PLZ"
+              :type="'number'"
+              :rules="[rules.required, rules.zip]"
+              :error-messages="useErrors().checkAndMapErrors('zip', errors)"
+              disabled
+            />
+            <v-select
+              hide-details="auto"
+              v-model="careFacilityTown"
+              :items="getTownsByCommunityId(careFacilityCommunityId)"
+              item-title="name"
+              item-value="name"
+              label="Ort"
+              :rules="[rules.required]"
+            />
+          </div> -->
+          <v-checkbox v-model="privacyAccepted" :rules="[rules.required]">
             <template v-slot:label>
               <div>
                 Ich stimme der
                 <v-tooltip bottom>
                   <template v-slot:activator="{ props }">
-                    <a
-                      target="_blank"
-                      href="/privacy_policy"
-                      @click.stop
-                      v-bind="props"
-                    >
+                    <a target="_blank" href="/privacy_policy" @click.stop v-bind="props">
                       <u>Datenschutzerklärung</u>
                     </a>
                   </template>
@@ -102,12 +100,7 @@
                 <span> und den </span>
                 <v-tooltip bottom>
                   <template v-slot:activator="{ props }">
-                    <a
-                      target="_blank"
-                      href="/terms_of_use"
-                      @click.stop
-                      v-bind="props"
-                    >
+                    <a target="_blank" href="/terms_of_use" @click.stop v-bind="props">
                       <u>Nutzungsbedingungen</u>
                     </a>
                   </template>
@@ -118,9 +111,10 @@
             </template>
           </v-checkbox>
           <v-btn color="primary" block depressed @click="register" :disabled="!formValidated">Registrieren</v-btn>
-        </div>
-        <div v-else align="center">
-          Deine Registrierung war erfolgreich.<br/>Wir haben Dir soeben eine E-Mail mit weiteren Anweisungen und einem temporären Passwort geschickt.
+        </v-form>
+        <div v-if="registerSuccessful" align="center">
+          Deine Registrierung war erfolgreich.<br />Wir haben Dir soeben eine E-Mail mit weiteren Anweisungen und einem
+          temporären Passwort geschickt.
         </div>
       </v-card>
     </v-col>
@@ -128,36 +122,38 @@
 </template>
 
 <script lang="ts" setup>
-import { ResultStatus, ServerCallResult } from '@/types/serverCallResult'
+import { ResultStatus, ServerCallResult } from "@/types/serverCallResult";
 import { rules } from "../data/validationRules";
-import axios from 'axios'
+import axios from "axios";
+import { VForm } from "vuetify/lib/components/index.mjs";
 
-const careFacilityName = ref('')
-const email = ref('')
-const careFacilityZip = ref('')
-const careFacilityTown = ref('')
-const careFacilityCommunityId = ref('')
-const firstname = ref('')
-const lastname = ref('')
-const phone = ref('')
-const loading = ref(false)
-const animated = ref(false)
-const errors = ref({})
-const registerSuccessful = ref(false)
-const privacyAccepted = ref(false)
+const careFacilityName = ref("");
+const email = ref("");
+const careFacilityZip = ref("");
+const careFacilityTown = ref("");
+const careFacilityCommunityId = ref("");
+const firstname = ref("");
+const lastname = ref("");
+const phone = ref("");
+const loading = ref(false);
+const animated = ref(false);
+const errors = ref({});
+const registerSuccessful = ref(false);
+const privacyAccepted = ref(false);
+const registerForm = ref<VForm>();
 
-const isValidEmail = (email:string) => {
-  const re = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/
+const isValidEmail = (email: string) => {
+  const re = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
   return re.test(email);
-}
+};
 
 const formValidated = computed(() => {
-  return careFacilityName.value.length > 0 && firstname.value.length > 0 && lastname.value.length > 0 && phone.value.length > 0 && isValidEmail(email.value) && privacyAccepted.value
-})
+  return registerForm.value?.isValid;
+});
 
 const register = async () => {
-  loading.value = true
-  errors.value = {}
+  loading.value = true;
+  errors.value = {};
   const data = {
     email: email.value,
     firstname: firstname.value,
@@ -167,41 +163,39 @@ const register = async () => {
     care_facility_name: careFacilityName.value,
     care_facility_zip: careFacilityZip.value,
     care_facility_town: careFacilityTown.value,
-    care_facility_community_id: careFacilityCommunityId.value
-  }
-
-  const {data: result} = await axios.post<ServerCallResult>("/api/register_with_facility", {data});
-
-  if (result.status === ResultStatus.SUCCESSFUL) {
-    localStorage.setItem('health_platform._remembered_email', email.value)
-    registerSuccessful.value = true
-  } else {
-    errors.value = { errors: [{ field_name: 'email', code: 'register.failed' }] }
-    loading.value = false
-
-    // animate shake
-    animated.value = true
-    setTimeout(() => {
-      animated.value = false
-    }, 1000)
-  }
-}
-
-  const communitiesApi = useCollectionApi();
-  communitiesApi.setBaseApi(usePublicApi());
-  communitiesApi.setEndpoint(`communities`);
-  const communities = communitiesApi.items;
-
-  const getCommunities = async () => {
-    await communitiesApi.retrieveCollection();
+    care_facility_community_id: careFacilityCommunityId.value,
   };
 
-  const getTownsByCommunityId = (communityId: string) => {
-    const found = communities.value.find(
-      (community: any) => community.id === communityId
-    );
-     if (found) {
-      careFacilityZip.value = found.zip;
+  const { data: result } = await axios.post<ServerCallResult>("/api/register_with_facility", { data });
+
+  if (result.status === ResultStatus.SUCCESSFUL) {
+    localStorage.setItem("health_platform._remembered_email", email.value);
+    registerSuccessful.value = true;
+  } else {
+    errors.value = { errors: [{ field_name: "email", code: "register.failed" }] };
+    loading.value = false;
+
+    // animate shake
+    animated.value = true;
+    setTimeout(() => {
+      animated.value = false;
+    }, 1000);
+  }
+};
+
+const communitiesApi = useCollectionApi();
+communitiesApi.setBaseApi(usePublicApi());
+communitiesApi.setEndpoint(`communities`);
+const communities = communitiesApi.items;
+
+const getCommunities = async () => {
+  await communitiesApi.retrieveCollection();
+};
+
+const getTownsByCommunityId = (communityId: string) => {
+  const found = communities.value.find((community: any) => community.id === communityId);
+  if (found) {
+    careFacilityZip.value = found.zip;
     return found.towns;
   } else {
     [];
@@ -210,31 +204,42 @@ const register = async () => {
 
 onMounted(() => {
   getCommunities();
-  const rememberedEmail = localStorage.getItem('health_platform._remembered_email')
+  const rememberedEmail = localStorage.getItem("health_platform._remembered_email");
   if (rememberedEmail) {
     setTimeout(() => {
-      email.value = rememberedEmail
-    }, 300)
+      email.value = rememberedEmail;
+    }, 300);
   }
-})
+
+  registerForm.value?.validate()
+});
 </script>
 
 <style lang="css" scoped>
+.card {
+  padding: 1rem;
+}
+
 .shake {
-  animation: shake 0.82s cubic-bezier(.36,.07,.19,.97) both;
+  animation: shake 0.82s cubic-bezier(0.36, 0.07, 0.19, 0.97) both;
   transform: translate3d(0, 0, 0);
 }
 @keyframes shake {
-  10%, 90% {
+  10%,
+  90% {
     transform: translate3d(-1px, 0, 0);
   }
-  20%, 80% {
+  20%,
+  80% {
     transform: translate3d(2px, 0, 0);
   }
-  30%, 50%, 70% {
+  30%,
+  50%,
+  70% {
     transform: translate3d(-4px, 0, 0);
   }
-  40%, 60% {
+  40%,
+  60% {
     transform: translate3d(4px, 0, 0);
   }
 }
