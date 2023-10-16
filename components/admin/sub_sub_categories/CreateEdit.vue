b5f5b1858a71ca9ffcaad7390d2a76b32a9c9595
Alexandre Laurenti <alexandre_laurenti@hotmail.com>
2023-10-16 18:19:25 +0200

    - added button text for sub_sub_categories

--- components/admin/sub_sub_categories/CreateEdit.vue
+++ components/admin/sub_sub_categories/CreateEdit.vue
@@ -6,7 +6,10 @@
           v-model="slotProps.item.name"
           hide-details="auto"
           label="Bezeichnung"
-          :error-messages="useErrors().checkAndMapErrors('name', slotProps.errors)"
+          :rules="[rules.required]"
+          :error-messages="
+            useErrors().checkAndMapErrors('name', slotProps.errors)
+          "
         />
       </div>
       <div class="field mb-5">
@@ -17,7 +20,9 @@
           hide-details="auto"
           label="Beschreibung"
           :rules="[rules.length]"
-          :error-messages="useErrors().checkAndMapErrors('Beschreibung', slotProps.errors)"
+          :error-messages="
+            useErrors().checkAndMapErrors('Beschreibung', slotProps.errors)
+          "
         />
       </div>
       <div class="field mb-5">
@@ -46,7 +51,13 @@
         </div>
       </div>
 
-      <v-alert v-if="slotProps.item.url_kind === 'internal'" type="info" density="compact" closable class="mb-5">
+      <v-alert
+        v-if="slotProps.item.url_kind === 'internal'"
+        type="info"
+        density="compact"
+        closable
+        class="mb-5"
+      >
         "bitte z.B.:
         "/public/categories/95bd1f21-800a-47a6-bb0c-afba56f33619?sub_category_id=d41393e1-20f2-4fee-8872-c275201dd26d"
       </v-alert>
@@ -57,14 +68,31 @@
           v-model="slotProps.item.url"
           hide-details="auto"
           label="Url"
+          
           :rules="[rules.isUrl]"
-          :error-messages="useErrors().checkAndMapErrors('name', slotProps.errors)"
+          :error-messages="
+            useErrors().checkAndMapErrors('name', slotProps.errors)
+          "
         /><v-text-field
           v-else
           v-model="slotProps.item.url"
           hide-details="auto"
           label="Url"
-          :error-messages="useErrors().checkAndMapErrors('name', slotProps.errors)"
+          :rules="[rules.required]"
+          :error-messages="
+            useErrors().checkAndMapErrors('name', slotProps.errors)
+          "
+        />
+      </div>
+      <div class="field mb-5">
+        <v-text-field
+          v-model="slotProps.item.button_text"
+          hide-details="auto"
+          label="Button Text"
+          :rules="[rules.required]"
+          :error-messages="
+            useErrors().checkAndMapErrors('name', slotProps.errors)
+          "
         />
       </div>
     </v-card-text>
@@ -77,6 +105,9 @@ const status = ref([
   { name: "internal", id: "internal" },
 ]);
 const setLogo = (image: any) => {
-  useNuxtApp().$bus.$emit("setPayloadFromSlotChild", { name: "file", value: image });
+  useNuxtApp().$bus.$emit("setPayloadFromSlotChild", {
+    name: "file",
+    value: image,
+  });
 };
 </script>
