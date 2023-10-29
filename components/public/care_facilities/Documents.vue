5f0d3b2ffeb767c22f91cbd17570409da7e1e803
Alexandre Laurenti <alexandre_laurenti@hotmail.com>
2023-10-29 21:03:36 +0100

    - edit font sizes and color

--- components/public/care_facilities/Documents.vue
+++ components/public/care_facilities/Documents.vue
@@ -3,7 +3,7 @@
     class="box flex-column is-dark-grey font-weight-medium pa-5"
     v-if="careFacility?.sanitized_documents?.filter((doc:any) => doc.tag === 'documents')?.length > 0"
   >
-    <h2 class="is-primary is-uppercase mb-6">Dokumente</h2>
+    <span class="is-primary is-uppercase general-font-size font-weight-medium mb-6">Dokumente</span>
     <div class="flex-column">
       <div
         v-for="document in careFacility.sanitized_documents.filter((doc:any) => doc.tag === 'documents')"
@@ -11,7 +11,7 @@
       >
       <img class="mr-2 icon" :src="iconDownload" />
         <span class="d-flex align-center">
-          <a :href="document.url" target="_blank"
+          <a class="general-font-size is-dark-grey" :href="document.url" target="_blank"
             >{{
               document.name.replace("-documents", ".pdf")
             }}</a
