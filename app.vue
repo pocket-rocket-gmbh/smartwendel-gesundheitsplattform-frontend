06bd1f04c2505e829756e60b8ba8749061b0ee8e
Alexandre Laurenti <alexandre_laurenti@hotmail.com>
2023-11-07 16:06:21 +0100

    - loading indicator before app fired

--- app.vue
+++ app.vue
@@ -1,9 +1,22 @@
 <template>
-  <NuxtLayout>
+  <NuxtLayout :class="[loading ? 'overlay' : '']">
     <NuxtPage />
   </NuxtLayout>
+  <LoadingSpinner class="text-spinner"  v-if="loading"></LoadingSpinner>
 </template>
 
+<script lang="ts" setup>
+const loading = ref(true);
+const setLoading = async() => {
+  setTimeout(() => {
+    loading.value = false;
+  }, 0);
+};
+
+onMounted(() => {
+  setLoading();
+});
+</script>
 <style lang="scss">
 @import "@/assets/sass/main.sass";
 
@@ -11,5 +24,30 @@
   body {
     font-size: 20px;
   }
+}
+
+
+.text-spinner {
+  position: fixed!important;
+  top: 50%!important;
+  left: 50%!important;
+  transform: translate(-50%, -50%)!important;
+  opacity: 1!important;
+}
+
+.spinner {
+  border-top-color: #8ab61d!important;
+}
+.overlay {
+  position: fixed;
+  width: 100%;
+  height: 100%;
+  top: 0;
+  left: 0;
+  right: 0;
+  bottom: 0;
+  background-color: rgba(0, 0, 0, 0.5);
+  z-index: 99;
+  opacity: 0;
 }
 </style>
