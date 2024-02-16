cff9cb34ea1e64ed342f9e3d8cd3d8d3215e3a3d
Alexandre Laurenti <alexandre_laurenti@hotmail.com>
2024-02-16 17:39:04 +0100

    - partial fix services filter

--- components/public/search/FilterSelectModal.vue
+++ components/public/search/FilterSelectModal.vue
@@ -9,64 +9,92 @@
       <template v-slot:activator="{ props }">
         <div v-bind="props">
           <div class="is-white d-flex align-end mt-10">
-            <img class="filter-icon" :src="iconFilter">
+            <img class="filter-icon" :src="iconFilter" />
           </div>
         </div>
       </template>
       <v-card>
         <v-toolbar dense fixed dark color="primary" class="modal-toolbar">
-          <v-btn icon v-if="filterStore.currentTags?.length" @click="filterStore.currentTags = []">
-            <v-icon>mdi-reload</v-icon>
-          </v-btn>
-          <v-toolbar-title class="d-flex justify-start">Leistung auswählen</v-toolbar-title>
-          <v-btn v-if="filterStore.currentTags?.length" variant="text" @click="dialog = false"> Fertig </v-btn>
-          <v-btn v-else variant="text" @click="dialog = false"> Schließen </v-btn>
+          <v-toolbar-title class="d-flex justify-start"
+            >Leistung auswählen</v-toolbar-title
+          >
+          <v-btn variant="text" @click="dialog = false"> Fertig </v-btn>
         </v-toolbar>
-        <div class="filter-tiles mt-5">
-          <template v-if="!loading">
-            <div v-for="filter in itemsForServiceList" class="filter-group">
-              <div v-for="item in filter.next" class="filter-selections mt-10">
-                <span v-if="item.next.length" class="text-h4 d-flex justify-center is-dark-grey">{{ item.title }}</span>
-                <v-row no-gutters class="fill-height item-row">
-                  <v-col
-                    cols="12"
-                    md="6"
-                    class="align-center column-items"
-                    v-for="subItem in item.next"
-                    v-auto-animate
+        <div class="filter-tiles">
+          <div
+            v-for="filter in availableItemsForServiceList"
+            class="filter-group"
+          >
+            <div v-for="item in filter.next" class="mt-5 filter-selections">
+              <span
+                v-if="
+                  item.next.length &&
+                  item.next.reduce(
+                    (acc, subItem) =>
+                      acc + parseInt(subItem.care_facilities_count),
+                    0
+                  )
+                "
+                class="general-font-size font-weight-bold is-dark-grey"
+                >{{ item.title }}</span
+              >
+
+              <div
+                class="align-center column-items pr-1 pt-1"
+                v-for="subItem in item.next.filter(
+                  (subItem) => parseInt(subItem.care_facilities_count) > 0
+                )"
+              >
+                <div
+                  v-if="subItem"
+                  class="d-flex align-center"
+                  :class="{
+                    selected:
+                      isSelectedTagNext(subItem) ||
+                      expandedItemIds.includes(subItem.id),
+                  }"
+                  @click="toggleSelection(subItem)"
+                >
+                  <v-btn
+                    class="options-select general-font-size ma-2 text-none font-weight-light"
+                    lang="de"
                   >
-                    <div
-                      class="filter-tile pa-5 word-break general-font-size"
-                      lang="de"
-                      :class="{
-                        selected:
-                          isSelectedTagNext(subItem) ||
-                          expandedItemIds.includes(subItem.id),
-                      }"
-                      @click="toggleSelection(subItem)"
+                    {{ subItem.title }}
+                  </v-btn>
+                  <span v-if="subItem.next.length">
+                    <v-icon
+                      class="is-clickable"
+                      v-if="expandedItemIds.includes(subItem.id)"
+                      size="x-large"
+                      >mdi-menu-up</v-icon
                     >
-                      {{ subItem.title }}
-                    </div>
-                    <div
-                      v-if="subItem.next.length && expandedItemIds.includes(subItem.id)"
-                      class="tag-select"
-                      v-for="tag in subItem.next"
-                    >
-                      <v-divider></v-divider>
-                      <v-checkbox
-                        :class="{ selected: isSelected(tag.id) }"
-                        class="mb-n4"
-                        :label="tag.title"
-                        v-model="filterStore.currentTags"
-                        :value="tag.id"
-                      />
-                    </div>
-                  </v-col>
-                </v-row>
+                    <span v-else>
+                      <v-icon class="is-clickable" size="x-large"
+                        >mdi-menu-down</v-icon
+                      >
+                    </span>
+                  </span>
+                </div>
+
+                <div
+                  v-if="
+                    subItem.next.length && expandedItemIds.includes(subItem.id)
+                  "
+                  class="tag-select"
+                  v-for="tag in subItem.next"
+                >
+                  <v-divider></v-divider>
+                  <v-checkbox
+                    :class="{ selected: isSelected(tag.id) }"
+                    class="mb-n4"
+                    :label="tag.title"
+                    v-model="filterStore.currentServiceTags"
+                    :value="tag.id"
+                  />
+                </div>
               </div>
             </div>
-          </template>
-          <LoadingSpinner v-else> Filter werden geladen ... </LoadingSpinner>
+          </div>
         </div>
       </v-card>
     </v-dialog>
@@ -74,7 +102,11 @@
 </template>
 
 <script setup lang="ts">
-import { type Facility, type FilterKind, useFilterStore } from "~/store/searchFilter";
+import {
+  type Facility,
+  type FilterKind,
+  useFilterStore,
+} from "~/store/searchFilter";
 import { ResultStatus } from "~/types/serverCallResult";
 import type { CollapsibleListItem } from "../../../types/collapsibleList";
 import iconFilter from "@/assets/icons/icon_filter.svg";
@@ -86,101 +118,16 @@ const props = defineProps<{
 const filterStore = useFilterStore();
 
 const dialog = ref(false);
-const loading = ref(false);
 
-const itemsForServiceList = ref<CollapsibleListItem[]>([]);
+const availableItemsForServiceList = ref<CollapsibleListItem[]>([]);
 const expandedItemIds = ref([]);
 
 const api = useCollectionApi();
 api.setBaseApi(usePublicApi());
 api.setEndpoint("tag_categories");
 
-type FilterResponse = {
-  id: string;
-  name: string;
-  menu_order: number;
-  parent_id: string;
-};
-
-const getItemsAndNext = (
-  filter: FilterResponse,
-  arrayToAdd: CollapsibleListItem[],
-  layer: number,
-  allFilters: FilterResponse[]
-) => {
-  if (layer === 4) {
-    return;
-  }
-
-  const filterItem: CollapsibleListItem = {
-    id: filter.id,
-    title: filter.name,
-    menuOrder: filter.menu_order,
-    layer,
-    next: [],
-  };
-
-  arrayToAdd.push(filterItem);
-
-  const childFilterItems: FilterResponse[] = allFilters.filter(
-    (item) => item.parent_id === filter.id
-  );
-
-  if (!childFilterItems.length) {
-    return;
-  }
-
-  childFilterItems.forEach((childFilterItem) =>
-    getItemsAndNext(childFilterItem, filterItem.next || [], layer + 1, allFilters)
-  );
-};
-
-const getItems = async () => {
-  api.setEndpoint(`tag_categories`);
-
-  const options = {
-    page: 1,
-    per_page: 999,
-    sort_by: "menu_order",
-    sort_order: "asc",
-    searchQuery: null as any,
-    concat: false,
-    filters: [] as any,
-  };
-
-  const result = await api.retrieveCollection(options);
-
-  if (result.status === ResultStatus.FAILED) {
-    console.error(result);
-    return;
-  }
-
-  const filters: any[] = result?.data?.resources?.filter(
-    (item: Facility) => props.filterKind === item.kind
-  );
-  if (!filters) {
-    console.error("No filters!");
-    return;
-  }
-
-  const serviceFilters = filters.filter(
-    (filter) => filter.filter_type === "filter_service"
-  );
-  const allFilters = await filterStore.loadAllFilters();
-
-  const tmpItemsForServiceList: CollapsibleListItem[] = [];
-
-  serviceFilters.forEach((filter) =>
-    getItemsAndNext(filter, tmpItemsForServiceList, 0, allFilters)
-  );
-
-  itemsForServiceList.value = [...tmpItemsForServiceList];
-};
-
-filterStore.currentTags;
-
 const isSelected = (itemId: string) => {
-  return filterStore.currentTags.includes(itemId);
+  return filterStore.currentServiceTags.includes(itemId);
 };
 
 const isSelectedTagNext = (tag: CollapsibleListItem) => {
@@ -193,7 +140,7 @@ const isSelectedTagNext = (tag: CollapsibleListItem) => {
 const toggleSelection = (item: CollapsibleListItem) => {
   if (item.next?.length) {
     const index = expandedItemIds.value.findIndex(
-      (expandedItemId) => expandedItemId === item.id
+      (expandedItemId: any) => expandedItemId === item.id
     );
 
     if (index === -1) {
@@ -205,16 +152,88 @@ const toggleSelection = (item: CollapsibleListItem) => {
   }
 
   if (isSelected(item.id)) {
-    filterStore.currentTags = filterStore.currentTags.filter((id) => id !== item.id);
+    filterStore.currentServiceTags = filterStore.currentServiceTags.filter(
+      (id: any) => id !== item.id
+    );
   } else {
-    filterStore.currentTags.push(item.id);
+    filterStore.currentServiceTags.push(item.id);
   }
 };
 
+const checkIfFiltersAreInFacilities = (
+  filters: CollapsibleListItem[],
+  filterIdsInFacility: string[]
+) => {
+  filters = filters.filter((currentFilter) => {
+    if (filterIdsInFacility.includes(currentFilter.id)) {
+      return true;
+    }
+
+    if (currentFilter.next?.length) {
+      currentFilter.next = checkIfFiltersAreInFacilities(
+        currentFilter.next,
+        filterIdsInFacility
+      );
+      return currentFilter.next.length;
+    }
+
+    return false;
+  });
+  return filters;
+};
+
+watch(
+  () => filterStore.filteredResults,
+  (newValue: any) => {
+    availableItemsForServiceList.value = [
+      ...deepToRaw(filterStore.allServiceTags),
+    ];
+    checkIfFiltersAreInFacilities(
+      availableItemsForServiceList.value,
+      newValue.map((facility: any) => facility.tag_category_ids).flat()
+    );
+
+    filterStore.loadAllFacilityFilters();
+    emitFiltersUpdated();
+  },
+  {
+    deep: true,
+  }
+);
+
+const emitFiltersUpdated = () => {
+  useNuxtApp().$bus.$on("filtersUpdated", () => {
+    availableItemsForServiceList.value = [
+      ...deepToRaw(filterStore.allServiceTags),
+    ];
+    checkIfFiltersAreInFacilities(
+      availableItemsForServiceList.value,
+      filterStore.allResults
+        .map((facility: any) => facility.tag_category_ids)
+        .flat()
+    );
+  });
+};
+watch(
+  () => filterStore.allServiceTags,
+  () => {
+    availableItemsForServiceList.value = [
+      ...deepToRaw(filterStore.allServiceTags),
+    ];
+    checkIfFiltersAreInFacilities(
+      availableItemsForServiceList.value,
+      filterStore.allResults
+        .map((facility: any) => facility.tag_category_ids)
+        .flat()
+    );
+  },
+  {
+    deep: true,
+  }
+);
+
 onMounted(async () => {
-  loading.value = true;
-  await getItems();
-  loading.value = false;
+  emitFiltersUpdated();
 });
 </script>
 
