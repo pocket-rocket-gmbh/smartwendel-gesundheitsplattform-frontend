4dc3b28935c7c742b4795febe758058349a8d834
Tobias Wälde <tw@pocket-rocket.io>
2024-02-20 12:03:53 +0100

    add opening hours

--- components/public/care_facilities/Right.vue
+++ components/public/care_facilities/Right.vue
@@ -1,147 +1,143 @@
 <template>
-  <div class="box flex-column is-dark-grey pa-5">
-    <span class="is-primary general-font-size is-uppercase font-weight-medium mb-6"
-      >Kontakt und Infos</span
-    >
-    <div
-      class="py-3 d-flex flex-column justify-center"
-      v-if="careFacility?.kind !== 'facility'"
-    >
-      <span class="d-flex align-center">
-        <img class="mr-2 icon" :src="facilityIcon" />
-        <a
-          class="is-dark-grey general-font-size pt-1"
-          :href="`/public/care_facilities/${careFacility?.user_care_facility.id}`"
-          target="blank"
-        >
-          {{ careFacility?.user_care_facility.name }}</a
-        >
-      </span>
-    </div>
-    <div v-if="careFacility?.street || careFacility?.zip || careFacility?.town">
-      <div class="py-3">
-        <span class="d-flex align-center">
-          <img class="mr-2 icon" :src="iconAddress" />
-          <span class="is-dark-grey general-font-size">{{ careFacility.street }}</span>
-        </span>
-        <div v-if="careFacility?.additional_address_info" class="aditional-info">
-          <v-icon class="mr-2"></v-icon>
-          <span class="mr-2 is-dark-grey general-font-size">
-            {{ careFacility?.additional_address_info }}
-          </span>
-        </div>
+	<div class="box flex-column is-dark-grey pa-5">
+		<span class="is-primary general-font-size is-uppercase font-weight-medium mb-6">
+			Kontakt und Infos
+		</span>
+		<div class="py-3 d-flex flex-column justify-center" v-if="careFacility?.kind !== 'facility'">
+			<span class="d-flex align-center">
+				<img class="mr-2 icon" :src="facilityIcon" />
+				<a
+					class="is-dark-grey general-font-size pt-1"
+					:href="`/public/care_facilities/${careFacility?.user_care_facility.id}`"
+					target="blank"
+				>
+					{{ careFacility?.user_care_facility.name }}
+				</a>
+			</span>
+		</div>
+		<div v-if="careFacility?.street || careFacility?.zip || careFacility?.town">
+			<div class="py-3">
+				<span class="d-flex align-center">
+					<img class="mr-2 icon" :src="iconAddress" />
+					<span class="is-dark-grey general-font-size">{{ careFacility.street }}</span>
+				</span>
+				<div v-if="careFacility?.additional_address_info" class="aditional-info">
+					<v-icon class="mr-2"></v-icon>
+					<span class="mr-2 is-dark-grey general-font-size">
+						{{ careFacility?.additional_address_info }}
+					</span>
+				</div>
 
-        <div>
-          <v-icon class="mr-2" color="primary"></v-icon>
-          <span class="is-dark-grey general-font-size"
-            >{{ careFacility.zip }} {{ careFacility.town }}</span
-          >
-        </div>
-      </div>
-    </div>
-    <div v-if="careFacility?.phone" class="py-3 d-flex flex-column justify-center">
-      <span class="d-flex align-center">
-        <img class="mr-2 icon" :src="iconPhone" />
-        <a class="is-dark-grey general-font-size" :href="`tel:${careFacility.phone}`">{{
-          careFacility.phone
-        }}</a>
-      </span>
-    </div>
-    <div v-if="careFacility?.email" class="py-3">
-      <span class="d-flex align-center">
-        <img class="mr-2 icon" :src="iconMail" />
-        <a
-          class="is-dark-grey general-font-size"
-          :href="`mailto:${careFacility.email}`"
-          >{{ careFacility.email }}</a
-        >
-      </span>
-    </div>
-    <div v-if="careFacility?.name_instructor" class="py-4">
-      <h3 class="is-primary is-uppercase general-font-size font-weight-medium mb-1">
-        <span v-if="careFacility?.kind === 'course'"> Kursleitung </span>
-        <span v-else> Veranstalter </span>
-      </h3>
-      <span class="is-dark-grey general-font-size">
-        {{ careFacility.name_instructor }}
-      </span>
-    </div>
-    <div class="mt-3" v-if="careFacility?.kind === 'facility' && careFacility.location">
-      <v-table density="compact">
-        <tbody>
-          <h3
-            class="is-primary is-dark-grey general-font-size font-weight-medium is-uppercase mb-1"
-          >
-            Öffnungszeiten
-          </h3>
-          <tr v-for="opening in careFacility.opening_hours" :key="opening.day">
-            <td class="py-3 is-primary is-dark-grey general-font-size">
-              {{ opening.day }}
-            </td>
-            <td class="py-3 is-dark-grey general-font-size" v-if="opening.hours.length">
-              {{ opening.hours }}
-            </td>
-            <td class="py-3 is-dark-grey general-font-size" v-else>keine Angabe</td>
-          </tr>
-        </tbody>
-      </v-table>
-    </div>
-    <div class="my-4" v-if="careFacility?.website">
-      <v-btn
-        append-icon="mdi-open-in-new"
-        class="general-font-size"
-        variant="outlined"
-        size="large"
-        rounded="pill"
-        color="primary"
-        width="100%"
-        :href="buttonHref"
-        target="_blank"
-      >
-        weitere Informationen
-      </v-btn>
-    </div>
-    <div class="my-4">
-      <v-btn
-        variant="outlined"
-        class="general-font-size"
-        size="large"
-        rounded="pill"
-        color="primary"
-        width="100%"
-        :href="`mailto:${careFacility?.email}`"
-      >
-        Kontakt aufnehmen
-      </v-btn>
-    </div>
-  </div>
+				<div>
+					<v-icon class="mr-2" color="primary"></v-icon>
+					<span class="is-dark-grey general-font-size">
+						{{ careFacility.zip }} {{ careFacility.town }}
+					</span>
+				</div>
+			</div>
+		</div>
+		<div v-if="careFacility?.phone" class="py-3 d-flex flex-column justify-center">
+			<span class="d-flex align-center">
+				<img class="mr-2 icon" :src="iconPhone" />
+				<a class="is-dark-grey general-font-size" :href="`tel:${careFacility.phone}`">
+					{{ careFacility.phone }}
+				</a>
+			</span>
+		</div>
+		<div v-if="careFacility?.email" class="py-3">
+			<span class="d-flex align-center">
+				<img class="mr-2 icon" :src="iconMail" />
+				<a class="is-dark-grey general-font-size" :href="`mailto:${careFacility.email}`">
+					{{ careFacility.email }}
+				</a>
+			</span>
+		</div>
+		<div v-if="careFacility?.name_instructor" class="py-4">
+			<h3 class="is-primary is-uppercase general-font-size font-weight-medium mb-1">
+				<span v-if="careFacility?.kind === 'course'">Kursleitung</span>
+				<span v-else>Veranstalter</span>
+			</h3>
+			<span class="is-dark-grey general-font-size">
+				{{ careFacility.name_instructor }}
+			</span>
+		</div>
+		<div class="mt-3" v-if="careFacility?.kind === 'facility' && careFacility.opening_hours">
+			<div style="display: flex; flex-direction: row; align-items: baseline">
+				<div class="is-primary-color">
+					<img class="mr-2 icon" :src="iconClock" />
+				</div>
+				<table style="width: 100%">
+					<tr v-for="opening in careFacility.opening_hours" :key="opening.day">
+						<td width="50%">{{ opening.day }}</td>
+						<td width="50%">
+							<span v-if="opening.hours">
+								{{ opening.hours }}
+							</span>
+							<span v-else>keine Angabe</span>
+						</td>
+					</tr>
+				</table>
+			</div>
+		</div>
+		<div class="my-4" v-if="careFacility?.website">
+			<v-btn
+				append-icon="mdi-open-in-new"
+				class="general-font-size"
+				variant="outlined"
+				size="large"
+				rounded="pill"
+				color="primary"
+				width="100%"
+				:href="buttonHref"
+				target="_blank"
+			>
+				weitere Informationen
+			</v-btn>
+		</div>
+		<div class="my-4">
+			<v-btn
+				variant="outlined"
+				class="general-font-size"
+				size="large"
+				rounded="pill"
+				color="primary"
+				width="100%"
+				:href="`mailto:${careFacility?.email}`"
+			>
+				Kontakt aufnehmen
+			</v-btn>
+		</div>
+	</div>
 </template>
 
 <script lang="ts" setup>
-import facilityIcon from "~/assets/icons/facilityTypes/facilities_green.svg";
-import iconPhone from "@/assets/icons/facilities/icon_phone.svg";
-import iconMail from "@/assets/icons/facilities/icon_mail.svg";
-import iconAddress from "@/assets/icons/facilities/icon_address.svg";
+import facilityIcon from '~/assets/icons/facilityTypes/facilities_green.svg';
+import iconPhone from '@/assets/icons/facilities/icon_phone.svg';
+import iconMail from '@/assets/icons/facilities/icon_mail.svg';
+import iconAddress from '@/assets/icons/facilities/icon_address.svg';
+import iconClock from '@/assets/icons/facilities/icon_clock.svg';
+import { IconClock } from '@tabler/icons-vue';
+
 const props = defineProps({
-  careFacility: {
-    type: Object,
-    required: true,
-  },
+	careFacility: {
+		type: Object,
+		required: true,
+	},
 });
 
 const buttonHref = computed(() => {
-  if (!props.careFacility) return null;
+	if (!props.careFacility) return null;
 
-  if (props.careFacility.website) {
-    if (
-      props.careFacility.website.includes("http://") ||
-      props.careFacility.website.includes("https://")
-    ) {
-      return props.careFacility.website;
-    } else return "https://" + props.careFacility.website;
-  }
+	if (props.careFacility.website) {
+		if (
+			props.careFacility.website.includes('http://') ||
+			props.careFacility.website.includes('https://')
+		) {
+			return props.careFacility.website;
+		} else return 'https://' + props.careFacility.website;
+	}
 
-  return null;
+	return null;
 });
 </script>
 
