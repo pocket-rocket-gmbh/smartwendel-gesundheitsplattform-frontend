bb97c95c288d1e06b94be1eeb035a1bffc6af47b
Tobias Wälde <tw@pocket-rocket.io>
2024-02-20 09:19:43 +0100

    fix imports

--- composables/server/interface.ts
+++ composables/server/interface.ts
@@ -1,67 +1,66 @@
-import axios, { AxiosInstance, AxiosRequestConfig, Method } from 'axios'
-import { ServerCallResult } from '@/types/serverCallResult'
-import { useSnackbar } from "~/composables/ui/snackbar";
+import axios from 'axios';
+import type { AxiosInstance, AxiosRequestConfig, Method } from 'axios';
+import { ServerCallResult } from '@/types/serverCallResult';
+import { useSnackbar } from '~/composables/ui/snackbar';
 
 export function useServerInterface() {
-  
-  const client: AxiosInstance =  axios.create()
-  const snackbar = useSnackbar()
+	const client: AxiosInstance = axios.create();
+	const snackbar = useSnackbar();
 
-  let domain = null
-  let headers = null
+	let domain = null;
+	let headers = null;
 
-  const setDomain = (domain_: string) => {
-    domain = domain_
-  }
+	const setDomain = (domain_: string) => {
+		domain = domain_;
+	};
 
-  const setHeaders = (headers_: Object) => {
-    headers = headers_
-  }
+	const setHeaders = (headers_: Object) => {
+		headers = headers_;
+	};
 
-  const call = async (method: Method, url: string, data?: any): Promise<ServerCallResult> => {
+	const call = async (method: Method, url: string, data?: any): Promise<ServerCallResult> => {
+		const requestConfig: AxiosRequestConfig = {
+			baseURL: domain,
+			url: url,
+			method: method,
+			data: data,
+			headers: headers,
+		};
 
-    const requestConfig: AxiosRequestConfig = {
-      baseURL: domain,
-      url: url,
-      method: method,
-      data: data,
-      headers: headers
-    }
+		try {
+			const response = await client.request(requestConfig);
+			return ServerCallResult.success(response.status, response.data);
+		} catch (error) {
+			snackbar.showError(getErrorMessage(error));
+			return ServerCallResult.error(
+				getErrorMessage(error),
+				error.response?.status,
+				error.response?.data
+			);
+		}
+	};
 
-    try {
-      const response = await client.request(requestConfig)
-      return ServerCallResult.success(response.status, response.data)
-    }
-    catch (error) {
-      snackbar.showError(getErrorMessage(error))
-      return ServerCallResult.error(getErrorMessage(error), error.response?.status, error.response?.data)
-    }
-  }
+	const getErrorMessage = (error: any) => {
+		let result: string;
 
-  const getErrorMessage = (error: any) => {
-    let result: string
+		if (error.response) {
+			if (error.response.data && error.response.data.msg) {
+				result = error.response.data.msg;
+			} else {
+				result = 'Ein Fehler ist aufgetreten';
+			}
+		} else if (error.code) {
+			result = error.code;
+		} else {
+			result = error.message;
+		}
 
-    if (error.response) {
-      if (error.response.data && error.response.data.msg) {
-        result = error.response.data.msg
-      }
-      else {
-        result = 'Ein Fehler ist aufgetreten'
-      }
-    }
-    else if (error.code) {
-      result = error.code
-    }
-    else {
-      result = error.message
-    }
+		return result;
+	};
 
-    return result
-  }
-
-  return {
-    setDomain,
-    setHeaders,
-    call
-  }
+	return {
+		setDomain,
+		setHeaders,
+		call,
+	};
 }
