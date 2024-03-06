8535a64b0a08a71b7aa7496ef939fff12de2aeef
Alexandre Laurenti Done <alexandre_laurenti@hotmail.com>
2024-03-06 15:38:53 +0100

    show traffic lights on front page

--- components/public/care_facilities/Right.vue
+++ components/public/care_facilities/Right.vue
@@ -1,19 +1,26 @@
 <template>
   <div class="box flex-column is-secondary-color pa-5">
-    <div class="is-secondary-color is-label-big general-font-size is-uppercase">Pflegeangebote</div>
+    <div class="is-secondary-color is-label-big general-font-size is-uppercase">
+      Pflegeangebote
+    </div>
     <div class="is-secondary-color general-font-size mb-6">
-      zuletzt aktualisiert: 02.12.2023
+      <pre>{{ careFacility.care_facility_tag_categories }}</pre>
     </div>
 
-    <v-row no-gutters v-for="places in freePlaces">
+    <v-row
+      no-gutters
+      v-for="(places, index) in careFacility.care_facility_tag_categories"
+      :key="index"
+    >
       <v-col class="d-flex flex-column justify-center align-center">
-        <div class="is-primary general-font-size">{{ places.title }}</div>
-        <div class="is-secondary-color general-font-size">
-          {{ places.description }}
+        <div class="is-primary general-font-size">
+          {{ places?.tag_category?.name }}
         </div>
         <div class="is-primary-color general-font-size mt-5">
-          <v-icon color="success">mdi-circle</v-icon>
-          <span class="ml-2">Plätze verfügbar</span>
+          <v-icon :color="capacityColor[index]">mdi-circle</v-icon>
+          <span class="ml-2" :style="{ color: capacityColor[index] }">
+            {{ capacityText[index] }}
+          </span>
         </div>
       </v-col>
       <v-divider class="my-5"></v-divider>
@@ -29,20 +36,35 @@ const props = defineProps({
   },
 });
 
-const freePlaces = [
-  {
-    title: "Betreutes Wohnen",
-    description: "Rollstuhlgerecht, Einzelappartement",
-  },
-  {
-    title: "Kurzzeitpflege",
-    description: "Rollstuhlgerecht, Einzelzimmer",
-  },
-  {
-    title: "Ambulante Pflege",
-    description: "",
-  },
-];
+const capacityColor = computed(() => {
+  return props.careFacility.care_facility_tag_categories.map((place: any) => {
+    const capacity = place.available_capacity;
+    if (capacity === 1) {
+      return "red";
+    } else if (capacity === 2) {
+      return "orange";
+    } else if (capacity === 3) {
+      return "green";
+    } else {
+      return "black";
+    }
+  });
+});
+
+const capacityText = computed(() => {
+  return props.careFacility.care_facility_tag_categories.map((place: any) => {
+    const capacity = place.available_capacity;
+    if (capacity === 1) {
+      return "Keine Plätze vorhanden";
+    } else if (capacity === 2) {
+      return "Platz auf Nachfrage";
+    } else if (capacity === 3) {
+      return "Plätze vorhanden";
+    } else {
+      return "Plätze unbekannt";
+    }
+  });
+});
 </script>
 
 <style lang="sass" scoped>
