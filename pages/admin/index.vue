752dcac28a3b54fab533edf52f1c73b49fee09f1
Alexandre Laurenti Done <alexandre_laurenti@hotmail.com>
2024-04-02 16:07:58 +0200

    - created statistics dashboard

--- pages/admin/index.vue
+++ pages/admin/index.vue
@@ -1,19 +1,20 @@
 <template>
   <div>
-    <span class="general-font-size is-dark-grey font-weight-bold"
-      >Dashboard</span
-    >
-<!--     <div v-for="item in items" :key="item.id">
+    <span class="general-font-size is-dark-grey font-weight-bold">Dashboard</span>
+    <div v-for="item in items" :key="item.id">
       <div class="d-flex align-center">
         <v-icon size="x-large" color="primary">{{ item.icon }}</v-icon>
-        <span class="text-h4 is-dark-grey">{{ item.title }}</span>
+        <span class="text-h4 ml-3 is-dark-grey">{{ item.title }}</span>
       </div>
-      <div class="d-flex align-center mt-5">
-        <div v-for="sub_item in item?.sub_items">
-          <AdminStatisticsBox :item="sub_item" />
-        </div>
+
+      <div>
+        <v-row>
+          <v-col md="3" v-for="sub_item in item?.sub_items">
+            <AdminStatisticsBox :item="sub_item" :loading="loading" />
+          </v-col>
+        </v-row>
       </div>
-    </div> -->
+    </div>
   </div>
 </template>
 
@@ -22,7 +23,37 @@ definePageMeta({
   layout: "admin",
 });
 
-const items = ref([
+const loading = ref(false);
+
+type DashboardItem = {
+  title: string;
+  icon: string;
+  id: number;
+  sub_items: {
+    title: string;
+    content: any;
+  }[];
+};
+
+const facilities = ref([]);
+
+const getCoursesArticles = async () => {
+  loading.value = true;
+  const options = {
+    page: 1,
+    per_page: 10000,
+  };
+
+  const api = useCollectionApi();
+  api.setBaseApi(usePrivateApi());
+  api.setEndpoint(`care_facilities`);
+
+  await api.retrieveCollection(options as any);
+  facilities.value = api.items.value as any;
+  loading.value = false;
+};
+
+const items = computed<DashboardItem[]>(() => [
   {
     title: "Einrichtungen",
     icon: "mdi-home",
@@ -30,15 +61,17 @@ const items = ref([
     sub_items: [
       {
         title: "Angemeldet",
-        props: 'registered'
+        content: facilities.value?.length,
       },
       {
         title: "Aktiv",
-        props: 'active'
+        content: facilities.value.filter((facility: any) => facility.is_active === true)
+          .length,
       },
       {
         title: "Inaktiv",
-        props: 'inactive'
+        content: facilities.value.filter((facility: any) => facility.is_active === false)
+          .length,
       },
     ],
   },
@@ -49,15 +82,23 @@ const items = ref([
     sub_items: [
       {
         title: "importierte Profile",
-        props: 'imported'
+        content: facilities.value.filter(
+          (facility: any) => facility?.user?.imported === true
+        ).length,
       },
       {
         title: "erfolgte Profilübernahmen",
-        props: 'successful-imported'
+        content: facilities.value.filter(
+          (facility: any) =>
+            facility?.user?.imported === true && !facility?.user?.onboarding_token?.length
+        ).length,
       },
       {
         title: "ausstehende Profilübernahmen",
-        props: 'pending-imported'
+        content: facilities.value.filter(
+          (facility: any) =>
+            facility?.user?.imported === true && facility?.user?.onboarding_token?.length
+        ).length,
       },
     ],
   },
@@ -65,17 +106,13 @@ const items = ref([
     title: "Verifizierungsanfragen",
     icon: "mdi-check-decagram-outline",
     id: 3,
-    sub_items: [
-      {props: 'pending-verification'},
-    ]
+    sub_items: [{ title: " gesendet", content: 0 }],
   },
   {
     title: "Datenaktualität",
     icon: "mdi-playlist-check",
     id: 4,
-    sub_items: [
-      {props: 'data-accuracy'},
-    ]
+    sub_items: [{ title: "Aktuell", content: 0 }],
   },
   {
     title: "Aktivität",
@@ -84,17 +121,27 @@ const items = ref([
     sub_items: [
       {
         title: "Aktive Veranstaltungen",
-        props: 'active-events'
+        content: facilities.value.filter(
+          (facility: any) => facility?.kind === "event" && facility.is_active === true
+        ).length,
       },
       {
         title: "Aktive Kurse",
-        props: 'active-courses'
+        content: facilities.value.filter(
+          (facility: any) => facility?.kind === "course" && facility.is_active === true
+        ).length,
       },
       {
         title: "Aktive Beiträge",
-        props: 'active-articles'
+        content: facilities.value.filter(
+          (facility: any) => facility?.kind === "news" && facility.is_active === true
+        ).length,
       },
     ],
   },
 ]);
+
+onMounted(async () => {
+  await getCoursesArticles();
+});
 </script>
