53c4268d5ea6a5a6cca0200b8eb4ac60b0a33fc1
Alexandre Laurenti Done <alexandre_laurenti@hotmail.com>
2024-04-09 15:51:11 +0200

    - new layout for inprint informations on facility page

--- components/public/care_facilities/InprintInformations.vue
+++ components/public/care_facilities/InprintInformations.vue
@@ -17,20 +17,65 @@
           <span class="is-primary font-weight-bold">Vertretungsberechtigte Person</span>
           <div>{{ facility.authorized_represent_name }}</div>
         </div>
-        <v-divider v-if="facility?.commercial_register_number" class="my-2"></v-divider>
-        <div v-if="facility.commercial_register_number">
-          <span class="is-primary font-weight-bold">Registernummer</span>
-          <div>{{ facility.commercial_register_number }}</div>
+        <v-divider
+          v-if="facility.commercial_register_number || facility.register_court"
+          class="my-2"
+        ></v-divider>
+
+        <div v-if="facility.commercial_register_number || facility.register_court">
+          <span class="general-font-size is-dark-grey font-weight-bold"
+            >Weitere Angaben gemäß § 5 TMG</span
+          >
+          <div>
+            <span
+              class="is-primary font-weight-bold"
+              v-if="facility.commercial_register_number"
+              >Registernummer:
+            </span>
+            <span>{{ facility.commercial_register_number }}</span>
+          </div>
+          <div v-if="facility.register_court">
+            <span class="is-primary font-weight-bold">Registergericht: </span>
+            <span>{{ facility.register_court }}</span>
+          </div>
+        </div>
+        <v-divider v-if="facility.management_name" class="my-2"></v-divider>
+        <div v-if="facility.management_name">
+          <span class="is-primary font-weight-bold"> Geschäftsführung</span>
+          <div v-for="name in facility.management_name.split(',')" :key="name">
+            {{ name.trim() }}
+          </div>
+        </div>
+        <v-divider v-if="facility.professional_title" class="my-2"></v-divider>
+        <div v-if="facility.professional_title">
+          <div class="general-font-size is-dark-grey font-weight-bold">
+            Für bestimmte Berufe
+          </div>
+          <span class="is-primary font-weight-bold">Gesetzliche Berufsbezeichnung</span>
+          <div>{{ facility.professional_title }}</div>
+        </div>
+        <v-divider v-if="facility.awarded_in" class="my-2"></v-divider>
+        <div v-if="facility.awarded_in">
+          <span class="is-primary font-weight-bold"> Verliehen in</span>
+          <div>{{ facility.awarded_in }}</div>
+        </div>
+        <v-divider
+          v-if="facility.responsible_supervisory_authority"
+          class="my-2"
+        ></v-divider>
+        <div v-if="facility.responsible_supervisory_authority">
+          <span class="is-primary font-weight-bold"> Zuständige Aufsichtsbehörde</span>
+          <div>{{ facility.responsible_supervisory_authority }}</div>
         </div>
         <v-divider v-if="facility.associated_chamber" class="my-2"></v-divider>
         <div v-if="facility.associated_chamber">
           <span class="is-primary font-weight-bold">Zuständige Kammer</span>
           <div>{{ facility.associated_chamber }}</div>
         </div>
-        <v-divider v-if="facility.professional_title" class="my-2"></v-divider>
-        <div v-if="facility.professional_title">
-          <span class="is-primary font-weight-bold">Gesetzliche Berufsbezeichnung</span>
-          <div>{{ facility.professional_title }}</div>
+        <v-divider v-if="facility.professional_regulations" class="my-2"></v-divider>
+        <div v-if="facility.professional_regulations">
+          <span class="is-primary font-weight-bold">Berufsrechtliche Regelung</span>
+          <div>{{ facility.professional_regulations }}</div>
         </div>
         <v-divider v-if="facility.tax_identification_number" class="my-2"></v-divider>
         <div v-if="facility.tax_identification_number">
@@ -38,51 +83,6 @@
             >Umsatzsteueridentifikationsnummer</span
           >
           <div>{{ facility.tax_identification_number }}</div>
-        </div>
-        <v-divider v-if="facility.professional_regulations" class="my-2"></v-divider>
-
-        <div v-if="facility.professional_regulations">
-          <span class="is-primary font-weight-bold"
-            >Berufsrechtliche Regelung</span
-          >
-          <div>{{ facility.professional_regulations }}</div>
-        </div>
-
-        //
-
-        <v-divider v-if="facility.register_court" class="my-2"></v-divider>
-        <div v-if="facility.register_court">
-          <span class="is-primary font-weight-bold">
-            Berufsrechtliche Regelungen (z.B. Berufsordnung für Ärzte als Link)</span
-          >
-          <div>{{ facility.register_court }}</div>
-        </div>
-
-        <v-divider v-if="facility.awarded_in" class="my-2"></v-divider>
-        <div v-if="facility.awarded_in">
-          <span class="is-primary font-weight-bold">
-            Berufsrechtliche Regelungen (z.B. Berufsordnung für Ärzte als Link)</span
-          >
-          <div>{{ facility.awarded_in }}</div>
-        </div>
-
-        <v-divider
-          v-if="facility.responsible_supervisory_authority"
-          class="my-2"
-        ></v-divider>
-        <div v-if="facility.responsible_supervisory_authority">
-          <span class="is-primary font-weight-bold">
-            Berufsrechtliche Regelungen (z.B. Berufsordnung für Ärzte als Link)</span
-          >
-          <div>{{ facility.responsible_supervisory_authority }}</div>
-        </div>
-
-        <v-divider v-if="facility.management_name" class="my-2"></v-divider>
-        <div v-if="facility.management_name">
-          <span class="is-primary font-weight-bold">
-            Berufsrechtliche Regelungen (z.B. Berufsordnung für Ärzte als Link)</span
-          >
-          <div>{{ facility.management_name }}</div>
         </div>
       </div>
     </div>
